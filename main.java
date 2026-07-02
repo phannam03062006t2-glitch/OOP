a2d67d4ab62ae781416943a10ea32878b4d25ccb
Phan Duy Nam <phannam03062006t2@gmail.com>
2026-07-03 02:41:48 +0700

    3

--- main.java
+++ main.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
 
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Hello PTIT.");
+        int w = sc.nextInt();
+        int h = sc.nextInt();
 
+        if (w <= 0 || h <= 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(2 * (w + h) + " " + w * h);
+        }
         sc.close();
     }
 }
