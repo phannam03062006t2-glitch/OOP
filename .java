3d3e401ef08b097a2b314fd1e0f99aedd0fb06db
Phan Duy Nam <phannam03062006t2@gmail.com>
2026-07-03 02:33:27 +0700

    2

---
+++ .java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.FileWriter;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Hello PTIT.");
+
+        sc.close();
+    }
+}
